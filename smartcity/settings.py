1782120bd66805d84a90a848bea477c661e074bd
harshavardhanBOMMALATA <hbommalata@gmail.com>
2025-12-11 18:07:55 +0530

    final commit for database connection

--- smartcity/settings.py
+++ smartcity/settings.py
@@ -82,11 +82,11 @@ if os.environ.get("RENDER_ENV") == "production":
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.environ.get('DB_NAME'),
-            'USER': os.environ.get('DB_USER'),
-            'PASSWORD': os.environ.get('DB_PASSWORD'),
-            'HOST': os.environ.get('DB_HOST'),
-            'PORT': os.environ.get('DB_PORT', '3306'),
+            'NAME': os.environ.get('railway'),
+            'USER': os.environ.get('root'),
+            'PASSWORD': os.environ.get('OtEgFptYGsbGVdogqkKsLicFhAGCtaxn'),
+            'HOST': os.environ.get('mysql.railway.internal'),
+            'PORT': os.environ.get('3306', '3306'),
             'OPTIONS': {
                 'charset': 'utf8mb4',
             },
