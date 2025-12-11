040ca03409538226d0e18338478f088cb483eddb
harshavardhanBOMMALATA <hbommalata@gmail.com>
2025-12-11 19:15:00 +0530

    error handling 3 db

--- smartcity/views.py
+++ smartcity/views.py
@@ -1,9 +1,4 @@
 from django.http import HttpResponse
-from django.db import connection
 
-def db_test(request):
-    try:
-        connection.cursor()
-        return HttpResponse("DATABASE CONNECTED SUCCESSFULLY ✔")
-    except Exception as e:
-        return HttpResponse(f"DATABASE ERROR ❌: {e}")
+def hello(request):
+    return HttpResponse("Hello ✔ Default view working")
