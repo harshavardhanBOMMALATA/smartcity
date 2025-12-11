dff93103b5fc96128a02c306ab5859c5a6203b74
harshavardhanBOMMALATA <hbommalata@gmail.com>
2025-12-11 18:59:21 +0530

    database connection error

--- database/views.py
+++ database/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.db import connection
 
-# Create your views here.
+def db_test(request):
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT 1;")
+        return HttpResponse("DATABASE CONNECTED ✔")
+    except Exception as e:
+        return HttpResponse("DATABASE ERROR ❌: " + str(e))
