1f7981ea59b76d148e7d5ec61bbcb223dd66205a
harshavardhanBOMMALATA <hbommalata@gmail.com>
2025-12-11 11:24:41 +0530

    update allowed hosts for deployments

--- smartcity/settings.py
+++ smartcity/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9^jnz=!g&#fv6#o3%9b)duqml+&mau71^dn5vkca1m1f79!&2#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
